0d06ee4ac79a169a65df64606d3a0d42cf4a1ab6
bouyg <bouyg@LAPTOP-I3FBDJLG>
2021-10-30 22:13:07 +0200

    Add project files.

---
+++ API/Controllers/SettingsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult InitGrid(string difficulty)
+        {
+            Console.WriteLine($"Difficulty : {difficulty}");
+            return Json("OK");
+        }
+    }
+}
---
+++ LightsOut/HttpService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace LightsOut
+{
+    class HttpService
+    {
+
+        private readonly HttpClient _httpClient;
+        private readonly string baseApiUrl = "http://localhost:51616";
+
+        public HttpService(HttpClient httpClient) => _httpClient = httpClient;
+
+        public async void GetSettings(string setting)
+        {
+            try
+            {
+                string result = await _httpClient.GetStringAsync($"{baseApiUrl}/InitGrid/easy");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+    }
+}
